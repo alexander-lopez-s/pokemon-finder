32b782479dae57f333b0d4ca1e371fffff47fa79
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-08-09 14:05:53 +0200

    On change event added

--- src/listeners/addListeners.js
+++ src/listeners/addListeners.js
@@ -2,13 +2,21 @@ import dom from "../dom.js";
 import displayPokemonData from "../handlers/handleSearch.js"
 
 const addListeners = () => {
-   dom.searchButton.addEventListener('click', displayPokemonData);
-   dom.searchBar.addEventListener('keydown', (event) => {
-   // Check if the pressed key is Enter (key code 13)
-   if (event.key === 'Enter' || event.keyCode === 13) {
+
+  // On 'click' event
+  dom.searchButton.addEventListener('click', displayPokemonData);
+
+  // On 'enter' event
+  dom.searchBar.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+      displayPokemonData();
+    }
+  });
+
+  // On 'change; event
+  dom.searchBar.addEventListener('change', () => {
     displayPokemonData();
-   }
- });
+  });
 };
 
 export default addListeners;
