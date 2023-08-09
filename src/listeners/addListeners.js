a532248667e6ad153d17bd06c39d81f4765ae389
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-08-09 10:40:15 +0200

    on Enter event added

--- src/listeners/addListeners.js
+++ src/listeners/addListeners.js
@@ -2,7 +2,13 @@ import dom from "../dom.js";
 import handleSearch from "../handlers/handleSearch.js"
 
 const addListeners = () => {
-   dom.searchButton.addEventListener('click', handleSearch)
-}
+   dom.searchButton.addEventListener('click', handleSearch);
+   dom.searchBar.addEventListener('keydown', (event) => {
+   // Check if the pressed key is Enter (key code 13)
+   if (event.key === 'Enter' || event.keyCode === 13) {
+     handleSearch();
+   }
+ });
+};
 
 export default addListeners;
